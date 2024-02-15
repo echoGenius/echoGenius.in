41c01d586dbd4c7742ac3226e6eb9261c34e0c43
praty89 <pratyushsakhlecha@gmail.com>
2024-02-15 15:51:12 +0530

    changed header items

--- public_html/includes/header.php
+++ public_html/includes/header.php
@@ -2,7 +2,7 @@
     <div class="container d-flex justify-content-between align-items-center">
 
       <div class="logo">
-        <!-- <h1 class="text-light"><a href="index.html"><span>Modsderna</span></a></h1> -->
+        <!-- <h1 class="text-light"><a href="index.html"><span>Test</span></a></h1> -->
         <!-- Uncomment below if you prefer to use an image logo -->
         <a href="#home"><img src="assets/img/logo.png" alt="" class="img-fluid"></a>
       </div>
@@ -15,10 +15,10 @@
         <ul>
           <li><a class=" <?= ($activePage == 'home') ? 'active':''; ?> " href="?page=home">Home</a></li>
           <li><a class=" <?= ($activePage == 'about') ? 'active':''; ?> " href="?page=about">About</a></li>
-          <li><a class=" <?= ($activePage == 'services') ? 'active':''; ?> " href="?page=services">Services</a></li>
-          <li><a class=" <?= ($activePage == 'products') ? 'active':''; ?> " href="?page=products">Products</a></li>
+          <!-- <li><a class=" <?= ($activePage == 'products') ? 'active':''; ?> " href="?page=products">Products</a></li> -->
+          <!-- <li><a class=" <?= ($activePage == 'services') ? 'active':''; ?> " href="?page=services">Services</a></li> -->
           <li><a class=" <?= ($activePage == 'team') ? 'active':''; ?> " href="?page=team">Team</a></li>
-          <li><a class=" <?= ($activePage == 'blog') ? 'active':''; ?> " href="?page=blog">Blog</a></li>
+          <!-- <li><a class=" <?= ($activePage == 'blog') ? 'active':''; ?> " href="?page=blog">Blog</a></li> -->
           <li><a class=" <?= ($activePage == 'contact') ? 'active':''; ?> " href="?page=contact">Contact Us</a></li>
         </ul>
         <i class="bi bi-list mobile-nav-toggle"></i>
