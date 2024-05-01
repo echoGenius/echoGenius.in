d3356004d71bca3287750074c041a729c011d287
praty89 <pratyushsakhlecha@gmail.com>
2024-05-02 01:13:08 +0530

    edited hero image and paragraph margin

--- public_html/sections/hero.php
+++ public_html/sections/hero.php
@@ -4,7 +4,9 @@
         <div class="col-xl-8">
         <h2>Welcome to <a style="color: #7ac2ff;">echoGenius</a></h2>
           <h3 style="color: #fff;">Creating a Digital <a style="color: #7ac2ff;">Revolution</a> for <br>Rural <a style="color: #7ac2ff;">Education</a>!</h3>
-          <p>Step into a future resonating with progress, where education undergoes a transformative evolution guided by technology. At <a style="color: #7ac2ff;">echoGenius</a>, we're more than just a company; we epitomize innovation and technological brilliance, shaping a landscape where groundbreaking ideas flourish and the echoes of progress reverberate throughout the education sector</p>
+          <p style="margin-top: 18%;">Step into a future resonating with progress, where education undergoes a transformative evolution guided by technology. 
+              At <a style="color: #7ac2ff;">echoGenius</a>, we're more than just a company; we epitomize innovation and technological brilliance, 
+              shaping a landscape where groundbreaking ideas flourish and the echoes of progress reverberate throughout the education sector</p>
         </div>
       </div>
     </div>
