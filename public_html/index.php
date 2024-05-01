1dd6ff8cc0de7174152ab1db199f391d28a1aa8a
praty89 <pratyushsakhlecha@gmail.com>
2024-05-02 01:30:33 +0530

    updated title

--- public_html/index.php
+++ public_html/index.php
@@ -5,7 +5,7 @@
   <meta charset="utf-8">
   <meta content="width=device-width, initial-scale=1.0" name="viewport">
 
-  <title>echoGenius | Igniting Innovation and Connectivity!</title>
+  <title>Creating a Digital Revolution for Rural Education</title>
   <meta content="" name="description">
   <meta content="" name="keywords">
 
